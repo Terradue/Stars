6d4ae3d9f67e05c9551ce8338a7ce54da1aad15d
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2020-10-01 16:41:21 +0200

    fixed relative path after extraction

--- src/Stars.Services/Supplier/GenericAsset.cs
+++ src/Stars.Services/Supplier/GenericAsset.cs
@@ -11,19 +11,21 @@ namespace Terradue.Stars.Services.Supplier
         private IRoute route;
         private readonly string label;
         private readonly IEnumerable<string> roles;
+        private Uri uri;
 
         public GenericAsset(IRoute route, string label, IEnumerable<string> roles)
         {
             this.route = route;
             this.label = label;
             this.roles = roles;
+            this.uri = route.Uri;
         }
 
         public string Label => label;
 
         public IEnumerable<string> Roles => roles;
 
-        public Uri Uri => route.Uri;
+        public Uri Uri => uri;
 
         public ContentType ContentType => route.ContentType;
 
@@ -34,6 +36,11 @@ namespace Terradue.Stars.Services.Supplier
         public IStreamable GetStreamable()
         {
             return route as IStreamable;
+        }
+
+        internal void SetUriRelativeTo(Uri uriRelative)
+        {
+            uri = uriRelative.MakeRelativeUri(uri);
         }
     }
 }
