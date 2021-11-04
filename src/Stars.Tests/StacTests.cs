21a4feb7412d3ed7fed05d2a947bb2653d78a4b7
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2021-11-04 12:09:02 +0100

    more tests

---
+++ src/Stars.Tests/StacTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using Stac;
+using Terradue.Stars.Services.Model.Stac;
+using Terradue.Stars.Services.Router;
+using Terradue.Stars.Services.Supplier.Destination;
+using Xunit;
+
+namespace Stars.Tests
+{
+    public class StacTests : IClassFixture<WebRequestFixture>
+    {
+        [Fact]
+        public void FolderRoute()
+        {
+            WebRoute route = WebRoute.Create(new Uri("file://" + Path.Join(Environment.CurrentDirectory, "../../../In/stacRoute/catalog.json")));
+            StacRouter router = new StacRouter(null); 
+            Assert.True(router.CanRoute(route));
+        }
+    }
+}
