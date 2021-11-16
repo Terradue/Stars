97e6e6872bf2e87cf04fec4f45ba80aa78aca0c0
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2021-11-16 15:51:57 +0100

    s3 parent

--- src/Stars.Services/Supplier/Destination/S3ObjectDestination.cs
+++ src/Stars.Services/Supplier/Destination/S3ObjectDestination.cs
@@ -14,14 +14,15 @@ namespace Terradue.Stars.Services.Supplier.Destination
     public class S3ObjectDestination : IDestination
     {
         private readonly Uri s3Uri;
-
+        private readonly IResource resource;
         private readonly char[] WRONG_FILENAME_STARTING_CHAR = new char[] { ' ', '.', '-', '$', '&' };
 
-        private S3ObjectDestination(Uri s3Uri)
+        private S3ObjectDestination(Uri s3Uri, IResource resource = null)
         {
             if (!s3Uri.Scheme.Equals("s3", StringComparison.CurrentCultureIgnoreCase))
                 throw new InvalidDataException("Only s3 URL supported");
             this.s3Uri = s3Uri;
+            this.resource = resource;
         }
 
         public string BucketName => S3UriParser.GetBucketName(s3Uri);
@@ -29,8 +30,8 @@ namespace Terradue.Stars.Services.Supplier.Destination
         public static S3ObjectDestination Create(string s3UriStr, IResource route = null)
         {
             Uri s3Uri = new Uri(s3UriStr);
-            var dest = new S3ObjectDestination(s3Uri);
-            if ( route != null) dest = (S3ObjectDestination)dest.To(route);
+            var dest = new S3ObjectDestination(s3Uri, route);
+            if (route != null) dest = (S3ObjectDestination)dest.To(route);
             return dest;
         }
 
@@ -41,47 +42,52 @@ namespace Terradue.Stars.Services.Supplier.Destination
             // S3WebRequest s3WebRequest = (S3WebRequest)WebRequest.Create(s3Uri);
         }
 
-        public IDestination To(IResource origin, string relPathFix = null)
+        public IDestination To(IResource subroute, string relPathFix = null)
         {
             // we first integrate the relPath
             string relPath = relPathFix ?? "";
 
             // we identify the filename
-            string filename = Path.GetFileName(origin.Uri.IsAbsoluteUri ? origin.Uri.LocalPath : origin.Uri.ToString());
-            if (origin.ContentDisposition != null && !string.IsNullOrEmpty(origin.ContentDisposition.FileName))
-                filename = origin.ContentDisposition.FileName;
+            string filename = Path.GetFileName(subroute.Uri.IsAbsoluteUri ? subroute.Uri.LocalPath : subroute.Uri.ToString());
+            if (subroute.ContentDisposition != null && !string.IsNullOrEmpty(subroute.ContentDisposition.FileName))
+                filename = subroute.ContentDisposition.FileName;
 
             // to avoid wrong filename such as '$value'
-            if (WRONG_FILENAME_STARTING_CHAR.Contains(filename[0]) && origin.ResourceType == ResourceType.Asset)
-                filename = (origin as IItem).Id + ".zip";
+            if (WRONG_FILENAME_STARTING_CHAR.Contains(filename[0]) && subroute.ResourceType == ResourceType.Asset)
+            {
+                if (resource != null && resource.ResourceType == ResourceType.Item)
+                    filename = (resource as IItem).Id + ".zip";
+                else
+                    filename = "asset.zip";
+            }
 
             // if the relPath requested is null, we will build one from the origin route to the new one
             if (relPathFix == null)
             {
-                if (origin.Uri.IsAbsoluteUri)
+                if (subroute.Uri.IsAbsoluteUri)
                 {
                     // Let's see if the 2 routes are relative
-                    var relUri = origin.Uri.MakeRelativeUri(Uri);
+                    var relUri = subroute.Uri.MakeRelativeUri(Uri);
                     // If not, let's see if they have a common pattern
                     if (relUri.IsAbsoluteUri)
                     {
-                        if (!string.IsNullOrEmpty(Path.GetDirectoryName(origin.Uri.AbsolutePath)) &&
+                        if (!string.IsNullOrEmpty(Path.GetDirectoryName(subroute.Uri.AbsolutePath)) &&
                             !string.IsNullOrEmpty(Path.GetDirectoryName(Uri.AbsolutePath)) &&
-                            Path.GetDirectoryName(origin.Uri.AbsolutePath).StartsWith(Path.GetDirectoryName(Uri.AbsolutePath)))
+                            Path.GetDirectoryName(subroute.Uri.AbsolutePath).StartsWith(Path.GetDirectoryName(Uri.AbsolutePath)))
                         {
-                            relPath = Path.GetDirectoryName(origin.Uri.AbsolutePath).Replace(Path.GetDirectoryName(Uri.AbsolutePath), "");
+                            relPath = Path.GetDirectoryName(subroute.Uri.AbsolutePath).Replace(Path.GetDirectoryName(Uri.AbsolutePath), "");
                         }
                     }
                     else
                         relPath = Path.GetDirectoryName(relUri.ToString());
                 }
                 else
-                    relPath = Path.GetDirectoryName(origin.Uri.ToString());
+                    relPath = Path.GetDirectoryName(subroute.Uri.ToString());
                 if (relPath == null || relPath.StartsWith(".."))
                     relPath = relPathFix ?? "";
             }
             string newFilePath = filename;
-            if ( !string.IsNullOrEmpty(relPath) )
+            if (!string.IsNullOrEmpty(relPath))
                 newFilePath = Path.Combine(relPath, filename);
             Uri newUri = new Uri(string.Format("s3:/" + Path.GetFullPath(Path.Combine(
                                                "/" + S3UriParser.GetBucketName(s3Uri),
