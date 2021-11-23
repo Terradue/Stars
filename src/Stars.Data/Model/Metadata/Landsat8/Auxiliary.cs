d07ddb43c05c866b7a6161fd1fd0912d7dd1fa1a
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2021-11-23 10:06:07 +0100

    L8 shape

---
+++ src/Stars.Data/Model/Metadata/Landsat8/Auxiliary.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Humanizer;
+using Kajabity.Tools.Java;
+
+namespace Terradue.Stars.Data.Model.Metadata.Landsat8
+{
+    public class Auxiliary : JavaProperties
+    {
+        public new string GetProperty(string key)
+        {
+            var r = base.GetProperty(key);
+            if (!string.IsNullOrEmpty(r)) r = r.Trim('"');
+            return r;
+        }
+
+        public bool IsLandsat8()
+        {
+            return SpaceCraftID == "LANDSAT_8";
+        }
+        //properties
+        public string SpaceCraftID => GetProperty("SPACECRAFT_ID");
+        public string SceneId => GetProperty("LANDSAT_SCENE_ID");
+        public string CreationDateTime => GetProperty("FILE_DATE") ?? GetProperty("DATE_PRODUCT_GENERATED");
+        public string AcquisitionDateTime => GetProperty("DATE_ACQUIRED") + "T" + GetProperty("SCENE_CENTER_TIME");
+
+        //platform
+        public string[] Instruments => GetProperty("SENSOR_ID").ToLower().Split('_');
+        public string CollectionNumber => GetProperty("COLLECTION_NUMBER");
+
+        public string Platform
+        {
+            get
+            {
+                return SpaceCraftID.ToLower().Hyphenate();
+            }
+        }
+        public double GSD
+        {
+            get
+            {
+                var cells = new List<double>{
+                    double.Parse(GetProperty("GRID_CELL_SIZE_PANCHROMATIC")),
+                    double.Parse(GetProperty("GRID_CELL_SIZE_REFLECTIVE")),
+                    double.Parse(GetProperty("GRID_CELL_SIZE_THERMAL"))
+                };
+                return cells.Min();
+            }
+        }
+
+        // projection
+        public string MapProjection => GetProperty("MAP_PROJECTION");
+        public string Orientation => GetProperty("ORIENTATION");
+        public int UTM_zone => int.Parse(GetProperty("UTM_ZONE"));
+
+        // orbit
+        public int OrbitNumber => int.Parse(GetProperty("WRS_PATH"));
+        public int Row => int.Parse(GetProperty("WRS_ROW"));
+        public int Path => int.Parse(GetProperty("WRS_PATH"));
+        public readonly string OrbitDirection = "descending";
+
+        // images attributes
+        public double SunAzimuth => double.Parse(GetProperty("SUN_AZIMUTH"));
+        public double SunElevation => double.Parse(GetProperty("SUN_ELEVATION"));
+        public readonly double OffNadirAngle = 0;
+        internal string ProductId => GetProperty("LANDSAT_PRODUCT_ID") ?? GetProperty("METADATA_FILE_NAME").Replace("_MTL.txt", "");
+
+        public int Panchromatic_Lines => int.Parse(GetProperty("PANCHROMATIC_LINES")); 
+        public int Panchromatic_Samples => int.Parse(GetProperty("PANCHROMATIC_SAMPLES")); 
+        public int Reflective_Lines => int.Parse(GetProperty("REFLECTIVE_LINES")); 
+        public int Reflective_Samples => int.Parse(GetProperty("REFLECTIVE_SAMPLES")); 
+        public int Thermal_Lines => int.Parse(GetProperty("THERMAL_LINES")); 
+        public int Thermal_Samples => int.Parse(GetProperty("THERMAL_SAMPLES")); 
+
+        public string CollectionCategory => GetProperty("COLLECTION_CATEGORY");
+
+        public double IncidenceAngle
+        {
+            get
+            {
+                switch (GetProperty("NADIR_OFFNADIR"))
+                {
+                    case "NADIR":
+                        return 0;
+                    case "OFF_NADIR":
+                        return double.Parse(GetProperty("ROLL_ANGLE"));
+                    default:
+                        return 0;
+                }
+            }
+        }
+
+        // EO
+        public double CloudCover
+        {
+            get
+            {
+                try
+                {
+                    return double.Parse(GetProperty("CLOUD_COVER"));
+                }
+                catch (Exception)
+                {
+                    return 0;
+                };
+            }
+        }
+
+        public double CloudCoverLand
+        {
+            get
+            {
+                try
+                {
+                    return double.Parse(GetProperty("CLOUD_COVER_LAND"));
+                }
+                catch (Exception)
+                {
+                    return 0;
+                };
+            }
+        }
+
+        // processing
+        public string DataType => GetProperty("DATA_TYPE");
+
+        //Geometry
+        public double LowerLeftCoordinateLat => double.Parse(GetProperty("CORNER_LL_LAT_PRODUCT"));
+        public double LowerLeftCoordinateLon => double.Parse(GetProperty("CORNER_LL_LON_PRODUCT"));
+        public double LowerRightCoordinateLat => double.Parse(GetProperty("CORNER_LR_LAT_PRODUCT"));
+        public double LowerRightCoordinateLon => double.Parse(GetProperty("CORNER_LR_LON_PRODUCT"));
+        public double UpperLeftCoordinateLat => double.Parse(GetProperty("CORNER_UL_LAT_PRODUCT"));
+        public double UpperLeftCoordinateLon => double.Parse(GetProperty("CORNER_UL_LON_PRODUCT"));
+        public double UpperRightCoordinateLat => double.Parse(GetProperty("CORNER_UR_LAT_PRODUCT"));
+        public double UpperRightCoordinateLon => double.Parse(GetProperty("CORNER_UR_LON_PRODUCT"));
+
+        // Band
+        public string Filename_Band1 => GetProperty("FILE_NAME_BAND_1");
+        public string Filename_Band2 => GetProperty("FILE_NAME_BAND_2");
+        public string Filename_Band3 => GetProperty("FILE_NAME_BAND_3");
+        public string Filename_Band4 => GetProperty("FILE_NAME_BAND_4");
+        public string Filename_Band5 => GetProperty("FILE_NAME_BAND_5");
+        public string Filename_Band6 => GetProperty("FILE_NAME_BAND_6");
+        public string Filename_Band7 => GetProperty("FILE_NAME_BAND_7");
+        public string Filename_Band8 => GetProperty("FILE_NAME_BAND_8");
+        public string Filename_Band9 => GetProperty("FILE_NAME_BAND_9");
+        public string Filename_Band10 => GetProperty("FILE_NAME_BAND_10");
+        public string Filename_Band11 => GetProperty("FILE_NAME_BAND_11");
+
+    }
+
+}
--- src/Stars.Data/Model/Metadata/Landsat8/Landsat8MetadataExtractor.cs
+++ src/Stars.Data/Model/Metadata/Landsat8/Landsat8MetadataExtractor.cs
@@ -7,8 +7,6 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
-using Humanizer;
-using Kajabity.Tools.Java;
 using Microsoft.Extensions.Logging;
 using ProjNet.CoordinateSystems;
 using Stac;
@@ -25,143 +23,6 @@ using Terradue.Stars.Services.Plugins;
 
 namespace Terradue.Stars.Data.Model.Metadata.Landsat8
 {
-
-    public class Auxiliary : JavaProperties
-    {
-        public new string GetProperty(string key)
-        {
-            var r = base.GetProperty(key);
-            if (!string.IsNullOrEmpty(r)) r = r.Trim('"');
-            return r;
-        }
-
-        public bool IsLandsat8()
-        {
-            return SpaceCraftID == "LANDSAT_8";
-        }
-        //properties
-        public string SpaceCraftID => GetProperty("SPACECRAFT_ID");
-        public string SceneId => GetProperty("LANDSAT_SCENE_ID");
-        public string CreationDateTime => GetProperty("FILE_DATE") ?? GetProperty("DATE_PRODUCT_GENERATED");
-        public string AcquisitionDateTime => GetProperty("DATE_ACQUIRED") + "T" + GetProperty("SCENE_CENTER_TIME");
-
-        //platform
-        public string[] Instruments => GetProperty("SENSOR_ID").ToLower().Split('_');
-        public string CollectionNumber => GetProperty("COLLECTION_NUMBER");
-
-        public string Platform
-        {
-            get
-            {
-                return SpaceCraftID.ToLower().Hyphenate();
-            }
-        }
-        public double GSD
-        {
-            get
-            {
-                var cells = new List<double>{
-                    double.Parse(GetProperty("GRID_CELL_SIZE_PANCHROMATIC")),
-                    double.Parse(GetProperty("GRID_CELL_SIZE_REFLECTIVE")),
-                    double.Parse(GetProperty("GRID_CELL_SIZE_THERMAL"))
-                };
-                return cells.Min();
-            }
-        }
-
-        // projection
-        public string MapProjection => GetProperty("MAP_PROJECTION");
-        public string Orientation => GetProperty("ORIENTATION");
-        public int UTM_zone => int.Parse(GetProperty("UTM_ZONE"));
-
-        // orbit
-        public int OrbitNumber => int.Parse(GetProperty("WRS_PATH"));
-        public int Row => int.Parse(GetProperty("WRS_ROW"));
-        public int Path => int.Parse(GetProperty("WRS_PATH"));
-        public readonly string OrbitDirection = "descending";
-
-        // images attributes
-        public double SunAzimuth => double.Parse(GetProperty("SUN_AZIMUTH"));
-        public double SunElevation => double.Parse(GetProperty("SUN_ELEVATION"));
-        public readonly double OffNadirAngle = 0;
-        internal string ProductId => GetProperty("LANDSAT_PRODUCT_ID") ?? GetProperty("METADATA_FILE_NAME").Replace("_MTL.txt", "");
-
-        public string CollectionCategory => GetProperty("COLLECTION_CATEGORY");
-
-        public double IncidenceAngle
-        {
-            get
-            {
-                switch (GetProperty("NADIR_OFFNADIR"))
-                {
-                    case "NADIR":
-                        return 0;
-                    case "OFF_NADIR":
-                        return double.Parse(GetProperty("ROLL_ANGLE"));
-                    default:
-                        return 0;
-                }
-            }
-        }
-
-        // EO
-        public double CloudCover
-        {
-            get
-            {
-                try
-                {
-                    return double.Parse(GetProperty("CLOUD_COVER"));
-                }
-                catch (Exception)
-                {
-                    return 0;
-                };
-            }
-        }
-
-        public double CloudCoverLand
-        {
-            get
-            {
-                try
-                {
-                    return double.Parse(GetProperty("CLOUD_COVER_LAND"));
-                }
-                catch (Exception)
-                {
-                    return 0;
-                };
-            }
-        }
-
-        // processing
-        public string DataType => GetProperty("DATA_TYPE");
-
-        //Geometry
-        public double LowerLeftCoordinateLat => double.Parse(GetProperty("CORNER_LL_LAT_PRODUCT"));
-        public double LowerLeftCoordinateLon => double.Parse(GetProperty("CORNER_LL_LON_PRODUCT"));
-        public double LowerRightCoordinateLat => double.Parse(GetProperty("CORNER_LR_LAT_PRODUCT"));
-        public double LowerRightCoordinateLon => double.Parse(GetProperty("CORNER_LR_LON_PRODUCT"));
-        public double UpperLeftCoordinateLat => double.Parse(GetProperty("CORNER_UL_LAT_PRODUCT"));
-        public double UpperLeftCoordinateLon => double.Parse(GetProperty("CORNER_UL_LON_PRODUCT"));
-        public double UpperRightCoordinateLat => double.Parse(GetProperty("CORNER_UR_LAT_PRODUCT"));
-        public double UpperRightCoordinateLon => double.Parse(GetProperty("CORNER_UR_LON_PRODUCT"));
-
-        // Band
-        public string Filename_Band1 => GetProperty("FILE_NAME_BAND_1");
-        public string Filename_Band2 => GetProperty("FILE_NAME_BAND_2");
-        public string Filename_Band3 => GetProperty("FILE_NAME_BAND_3");
-        public string Filename_Band4 => GetProperty("FILE_NAME_BAND_4");
-        public string Filename_Band5 => GetProperty("FILE_NAME_BAND_5");
-        public string Filename_Band6 => GetProperty("FILE_NAME_BAND_6");
-        public string Filename_Band7 => GetProperty("FILE_NAME_BAND_7");
-        public string Filename_Band8 => GetProperty("FILE_NAME_BAND_8");
-        public string Filename_Band9 => GetProperty("FILE_NAME_BAND_9");
-        public string Filename_Band10 => GetProperty("FILE_NAME_BAND_10");
-        public string Filename_Band11 => GetProperty("FILE_NAME_BAND_11");
-
-    }
 
     [PluginPriority(1000)]
     public class Landsat8MetadataExtraction : MetadataExtraction
@@ -214,7 +75,7 @@ namespace Terradue.Stars.Data.Model.Metadata.Landsat8
 
             // AddEoBandPropertyInItem(stacItem);
 
-            return StacItemNode.Create(stacItem, item.Uri);;
+            return StacItemNode.Create(stacItem, item.Uri); ;
 
         }
 
@@ -243,27 +104,27 @@ namespace Terradue.Stars.Data.Model.Metadata.Landsat8
         {
             string filename = Path.GetFileName(asset.Uri.ToString());
             if (filename.EndsWith("_B1.TIF", true, CultureInfo.InvariantCulture) || filename.EndsWith("_B01.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B1", asset, EoBandCommonName.coastal, 0.44, 0.02, 30);
+                AddbandAsset(stacItem, "B1", asset, EoBandCommonName.coastal, 0.44, 0.02, 30, auxiliary);
             if (filename.EndsWith("_B2.TIF", true, CultureInfo.InvariantCulture) || filename.EndsWith("_B02.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B2", asset, EoBandCommonName.blue, 0.48, 0.06, 30);
+                AddbandAsset(stacItem, "B2", asset, EoBandCommonName.blue, 0.48, 0.06, 30, auxiliary);
             if (filename.EndsWith("_B3.TIF", true, CultureInfo.InvariantCulture) || filename.EndsWith("_B03.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B3", asset, EoBandCommonName.green, 0.56, 0.06, 30);
+                AddbandAsset(stacItem, "B3", asset, EoBandCommonName.green, 0.56, 0.06, 30, auxiliary);
             if (filename.EndsWith("_B4.TIF", true, CultureInfo.InvariantCulture) || filename.EndsWith("_B04.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B4", asset, EoBandCommonName.red, 0.65, 0.04, 30);
+                AddbandAsset(stacItem, "B4", asset, EoBandCommonName.red, 0.65, 0.04, 30, auxiliary);
             if (filename.EndsWith("_B5.TIF", true, CultureInfo.InvariantCulture) || filename.EndsWith("_B05.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B5", asset, EoBandCommonName.nir, 0.86, 0.03, 30);
+                AddbandAsset(stacItem, "B5", asset, EoBandCommonName.nir, 0.86, 0.03, 30, auxiliary);
             if (filename.EndsWith("_B6.TIF", true, CultureInfo.InvariantCulture) || filename.EndsWith("_B06.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B6", asset, EoBandCommonName.swir16, 1.6, 0.08, 30);
+                AddbandAsset(stacItem, "B6", asset, EoBandCommonName.swir16, 1.6, 0.08, 30, auxiliary);
             if (filename.EndsWith("_B7.TIF", true, CultureInfo.InvariantCulture) || filename.EndsWith("_B07.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B7", asset, EoBandCommonName.swir22, 2.2, 0.2, 30);
+                AddbandAsset(stacItem, "B7", asset, EoBandCommonName.swir22, 2.2, 0.2, 30, auxiliary);
             if (filename.EndsWith("_B8.TIF", true, CultureInfo.InvariantCulture) || filename.EndsWith("_B08.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B8", asset, EoBandCommonName.pan, 0.59, 0.18, 15);
+                AddbandAsset(stacItem, "B8", asset, EoBandCommonName.pan, 0.59, 0.18, 15, auxiliary);
             if (filename.EndsWith("_B9.TIF", true, CultureInfo.InvariantCulture) || filename.EndsWith("_B09.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B9", asset, EoBandCommonName.cirrus, 1.37, 0.02, 30);
+                AddbandAsset(stacItem, "B9", asset, EoBandCommonName.cirrus, 1.37, 0.02, 30, auxiliary);
             if (filename.EndsWith("_B10.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B10", asset, EoBandCommonName.lwir11, 10.9, 0.8, 100);
+                AddbandAsset(stacItem, "B10", asset, EoBandCommonName.lwir11, 10.9, 0.8, 100, auxiliary);
             if (filename.EndsWith("_B11.TIF", true, CultureInfo.InvariantCulture))
-                AddbandAsset(stacItem, "B11", asset, EoBandCommonName.lwir12, 12, 1, 100);
+                AddbandAsset(stacItem, "B11", asset, EoBandCommonName.lwir12, 12, 1, 100, auxiliary);
             if (filename.EndsWith("_BQA.TIF", true, CultureInfo.InvariantCulture))
                 stacItem.Assets.Add("BQA", GetGenericAsset(stacItem, asset, "BQA"));
             if (filename.EndsWith("_MTL.txt", true, CultureInfo.InvariantCulture))
@@ -272,13 +133,20 @@ namespace Terradue.Stars.Data.Model.Metadata.Landsat8
                 stacItem.Assets.Add("angle_coefficient", GetGenericAsset(stacItem, asset, "metadata"));
         }
 
-        private void AddbandAsset(StacItem stacItem, string assetKey, IAsset asset, EoBandCommonName common_name, double center_wavelength, double full_width_half_max, double gsd)
+        private void AddbandAsset(StacItem stacItem, string assetKey, IAsset asset, EoBandCommonName common_name, double center_wavelength, double full_width_half_max, double gsd, Auxiliary auxiliary)
         {
             StacAsset stacAsset = StacAsset.CreateDataAsset(stacItem, asset.Uri,
                 new System.Net.Mime.ContentType(MimeTypes.GetMimeType(asset.Uri.ToString()))
             );
             stacAsset.Properties.AddRange(asset.Properties);
             var eo = stacItem.EoExtension();
+
+            stacAsset.ProjectionExtension().Shape = new int[2] { auxiliary.Reflective_Samples, auxiliary.Reflective_Lines };
+            if ( assetKey == "B8" )
+                stacAsset.ProjectionExtension().Shape = new int[2] { auxiliary.Panchromatic_Samples, auxiliary.Panchromatic_Lines };
+
+            if ( assetKey == "B10" || assetKey == "B11" )
+                stacAsset.ProjectionExtension().Shape = new int[2] { auxiliary.Thermal_Samples, auxiliary.Thermal_Lines };
 
             EoBandObject eoBandObject = new EoBandObject(assetKey, common_name);
             eoBandObject.CenterWavelength = center_wavelength;
@@ -332,7 +200,7 @@ namespace Terradue.Stars.Data.Model.Metadata.Landsat8
             stacItem.SetProperty("landsat:collection_number", auxiliary.CollectionNumber);
             stacItem.SetProperty("landsat:collection_category", auxiliary.CollectionCategory);
             stacItem.SetProperty("landsat:cloud_cover_land", auxiliary.CloudCoverLand);
-            
+
         }
 
         private void AddEoStacExtension(Auxiliary auxiliary, StacItem stacItem)
