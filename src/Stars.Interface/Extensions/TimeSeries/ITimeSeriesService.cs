bd9df1055484ac91e3ee88d836988656d604df7c
Emmanuel Mathot <emmanuel.mathot@terradue.com>
2022-10-31 12:00:09 +0100

    WIP

--- src/Stars.Interface/Extensions/TimeSeries/ITimeSeriesService.cs
+++ src/Stars.Interface/Extensions/TimeSeries/ITimeSeriesService.cs
@@ -1,11 +1,19 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Stac;
+using Stac.Common;
 
 namespace Terradue.Stars.Interface.Extensions.TimeSeries
 {
     public interface ITimeSeriesService
     {
-        Task<StacLink> CreateTimeSeriesAsync(IAbstractTimeSeriesCreationRequest ingestionRequest, CancellationToken cancellationToken);
+        Task<StacObjectLink> CreateNewTimeSeriesCollectionAsync(IAbstractTimeSeriesOperationRequest<StacCollection> request, CancellationToken cancellationToken);
+
+        Task<StacObjectLink> UpdateTimeSeriesCollectionAsync(IAbstractTimeSeriesOperationRequest<StacCollection> request, CancellationToken cancellationToken);
+
+        Task<StacObjectLink> PatchTimeSeriesCollectionAsync(IAbstractTimeSeriesOperationRequest<Patch> request, CancellationToken cancellationToken);
+
+        Task DeleteTimeSeriesCollectionAsync(IAbstractTimeSeriesOperationRequest<StacCollection> request, CancellationToken cancellationToken);
     }
 }
--- src/Stars.Services/ThirdParty/Egms/EgmsService.cs
+++ src/Stars.Services/ThirdParty/Egms/EgmsService.cs
@@ -1,17 +1,16 @@
 using System;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System.Collections.Generic;
 using Stac;
 using System.Linq;
-using Stac.Extensions.Projection;
-using Stac.Extensions.Eo;
-using Stac.Extensions.Raster;
 using Terradue.Stars.Interface.Extensions.TimeSeries;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Net.Http;
 using Newtonsoft.Json;
+using Terradue.Stars.Interface;
+using Stac.Common;
+using System.IO;
 
 namespace Terradue.Stars.Services.ThirdParty.Egms
 {
@@ -20,14 +19,17 @@ namespace Terradue.Stars.Services.ThirdParty.Egms
         private readonly IOptions<EgmsConfiguration> options;
         private readonly ILogger<EgmsService> logger;
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IResourceServiceProvider _resourceServiceProvider;
 
         public EgmsService(IOptions<EgmsConfiguration> options,
                                    ILogger<EgmsService> logger,
-                                   IHttpClientFactory httpClientFactory)
+                                   IHttpClientFactory httpClientFactory,
+                                   IResourceServiceProvider resourceServiceProvider)
         {
             this.options = options;
             this.logger = logger;
             _httpClientFactory = httpClientFactory;
+            _resourceServiceProvider = resourceServiceProvider;
         }
 
         public Uri Uri => Configuration?.BaseUri;
@@ -36,31 +38,88 @@ namespace Terradue.Stars.Services.ThirdParty.Egms
 
         public bool IsAvailable => Configuration != null;
 
-        public async Task<StacLink> CreateTimeSeriesAsync(IAbstractTimeSeriesCreationRequest ingestionRequest, CancellationToken cancellationToken)
+        public async Task<StacObjectLink> CreateNewTimeSeriesCollectionAsync(IAbstractTimeSeriesOperationRequest<StacCollection> ingestionRequest, CancellationToken cancellationToken)
         {
             var client = _httpClientFactory.CreateClient();
             client.BaseAddress = Configuration.BaseUri;
 
-            var request = new HttpRequestMessage(HttpMethod.Post, $"api/ns/${ingestionRequest.NamespaceId}/import");
-            EgmsTimeSeriesImportRequest postRequest = new EgmsTimeSeriesImportRequest()
-            {
-                Collection = ingestionRequest.Collection,
-                Url = ingestionRequest.Source,
-                TimeSeriesId = ingestionRequest.TimeSeriesId,
-                Format = ingestionRequest.Properties["format"]
-            };
-            request.Content = new StringContent(JsonConvert.SerializeObject(postRequest));
+            StacCollection collection = await ingestionRequest.GetOperationPayloadAsync(cancellationToken);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"api/ns/${ingestionRequest.NamespaceId}/cs");
+            request.Content = new StringContent(JsonConvert.SerializeObject(collection));
 
             var response = await client.SendAsync(request, cancellationToken);
             response.EnsureSuccessStatusCode();
 
-            return new StacLink(response.Headers.Location, "tsapi", "EGMS TimeSeries", "application/json");
+            return GetCollectionLink(await GetCollectionFromResponseAsync(response), response.Headers.Location);
+        }
+
+        private StacObjectLink GetCollectionLink(StacCollection collection, Uri uri)
+        {
+            StacObjectLink collectionLink = (StacObjectLink)StacLink.CreateObjectLink(collection, uri);
+            collectionLink.RelationshipType = "tsapi";
+            collectionLink.Title = collection.Title;
+            collectionLink.Type = collection.MediaType.ToString();
+            return collectionLink;
+        }
+
+        private async Task<StacCollection> GetCollectionFromResponseAsync(HttpResponseMessage response)
+        {
+            return StacConvert.Deserialize<StacCollection>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<StacObjectLink> UpdateTimeSeriesCollectionAsync(IAbstractTimeSeriesOperationRequest<StacCollection> updateRequest, CancellationToken cancellationToken)
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = Configuration.BaseUri;
+            StacCollection collection = await updateRequest.GetOperationPayloadAsync(cancellationToken);
+
+            var request = new HttpRequestMessage(HttpMethod.Put, $"api/ns/${updateRequest.NamespaceId}/cs/{collection.Id}");
+            request.Content = new StringContent(JsonConvert.SerializeObject(collection));
+
+            var response = await client.SendAsync(request, cancellationToken);
+            response.EnsureSuccessStatusCode();
+
+            collection = await GetCollectionFromResponseAsync(response);
+            var uri = new Uri(Configuration.BaseUri, $"api/ns/${updateRequest.NamespaceId}/cs/{collection.Id}");
+
+            return GetCollectionLink(collection, uri);
+        }
+
+        public async Task<StacObjectLink> PatchTimeSeriesCollectionAsync(IAbstractTimeSeriesOperationRequest<Patch> patchRequest, CancellationToken cancellationToken)
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = Configuration.BaseUri;
+            Patch patch = await patchRequest.GetOperationPayloadAsync(cancellationToken);
+
+            var request = new HttpRequestMessage(new HttpMethod("PATCH"), $"api/ns/${patchRequest.NamespaceId}/cs/{patchRequest.CollectionId}");
+            request.Content = new StringContent(JsonConvert.SerializeObject(patch));
+
+            var response = await client.SendAsync(request, cancellationToken);
+            response.EnsureSuccessStatusCode();
+
+            StacCollection collection = await GetCollectionFromResponseAsync(response);
+            var uri = new Uri(Configuration.BaseUri, $"api/ns/${patchRequest.NamespaceId}/cs/{collection.Id}");
+
+            return GetCollectionLink(collection, uri);
+        }
+
+        public async Task DeleteTimeSeriesCollectionAsync(IAbstractTimeSeriesOperationRequest<StacCollection> deleteRequest, CancellationToken cancellationToken)
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = Configuration.BaseUri;
+
+            var request = new HttpRequestMessage(HttpMethod.Delete, $"api/ns/${deleteRequest.NamespaceId}/cs/{deleteRequest.CollectionId}");
+
+            var response = await client.SendAsync(request, cancellationToken);
+            response.EnsureSuccessStatusCode();
+
         }
 
         public Uri GetEgmsApiLink(StacItem stacItem)
         {
             var link = stacItem.Links.Where(a => a.RelationshipType == "tsapi").First();
-            if(link != null) return link.Uri;
+            if (link != null) return link.Uri;
             return null;
         }
 
